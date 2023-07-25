e8234b92a7d7a596454b55ef1f283c3613423550
emon5419 <alauddinemon3@gmail.com>
2023-07-25 21:44:45 +0600

    Route-desgin

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
-import './App.css';
-import Layout from "./Components/Layout/Layout";
+import About from './Pages/About';
+import Policy from './Pages/Policy';
+import Contact from './Pages/Contact';
+import HomePage from './Pages/HomePage';
+import PageNotFound from './Pages/PageNotFound';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <>
-      <Layout>
-        <h1>Content</h1>
-      </Layout>
+
+      <Routes>
+
+        <Route path='/' element={<HomePage/>} />
+
+        <Route path='/about' element={<About/>} />
+
+        <Route path='/contact' element={<Contact />} />
+
+        <Route path='/policy' element={<Policy />} />
+
+        <Route path='/pageNotFound' element={<PageNotFound />} />
+
+      </Routes>
+
     </>
   );
 }
