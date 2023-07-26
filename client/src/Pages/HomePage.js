a561febc3facebd1f3fa43b280a326dd5abf7fa0
emon5419 <alauddinemon3@gmail.com>
2023-07-26 18:12:17 +0600

    everyPagesMetaTag

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -3,7 +3,7 @@ import Layout from '../Components/Layout/Layout';
 
 const HomePage = () => {
   return (
-    <Layout title={'Home-'}>
+    <Layout title={'Home-Ecommerce App'}>
       <h1>HomePage</h1>
     </Layout>
   )
