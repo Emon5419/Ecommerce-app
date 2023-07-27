10ba8e662af589450019e03e4d24905111542c80
emon5419 <alauddinemon3@gmail.com>
2023-07-27 13:54:17 +0600

    userDashboard

--- client/src/Pages/user/Dashboard.js
+++ client/src/Pages/user/Dashboard.js
@@ -1,9 +1,25 @@
 import React from "react";
-import Layout from "../../Components/Layout/Layout.js";
+import Layout from "../../Components/Layout/Layout";
+import UserMenu from "../../Components/Layout/UserMenu";
+import { useAuth } from "../../context/auth";
 const Dashboard = () => {
+   const [auth] = useAuth();
    return (
       <Layout title={"Dashboard"}>
-         <h1>Dashboard Page</h1>
+         <div className="container-flui m-3 p-3">
+            <div className="row">
+               <div className="col-md-3">
+                  <UserMenu />
+               </div>
+               <div className="col-md-9">
+                  <div className="card w-75 p-3">
+                     <h3>User Name: {auth?.user?.name}</h3>
+                     <h3>User E-mail: {auth?.user?.email}</h3>
+                     <h3>User Address: {auth?.user?.address}</h3>
+                  </div>
+               </div>
+            </div>
+         </div>
       </Layout>
    );
 };
