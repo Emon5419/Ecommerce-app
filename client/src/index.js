338aa600be9a3473d26b84a30ea7bcb51331c2d1
emon5419 <alauddinemon3@gmail.com>
2023-07-27 10:41:59 +0600

    Auth

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from 'react-router-dom';
+import { AuthProvider } from './context/auth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
+  <AuthProvider>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    </BrowserRouter>
+  </AuthProvider>
   
 );
 
