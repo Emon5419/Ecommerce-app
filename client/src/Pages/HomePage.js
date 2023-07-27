f3e32812e37e3a8ed728aacacb29896c6eba802c
emon5419 <alauddinemon3@gmail.com>
2023-07-27 10:49:18 +0600

    localStorageDataSetItem

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -5,7 +5,7 @@ import { useAuth } from "../context/auth";
 const HomePage = () => {
   const [auth, setAuth] = useAuth();
   return (
-    <Layout title={"Best offers "}>
+    <Layout title={"Home - Best Offers"}>
       <h1>HomePage</h1>
       <pre>{JSON.stringify(auth, null, 4)}</pre>
     </Layout>
